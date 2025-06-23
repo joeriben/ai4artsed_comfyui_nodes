51380c6b8f72fdc471c84236671842da3342ff4c
Prof. Dr. Benjamin Jörissen <joerissen@gmail.com>
2025-06-23 23:40:39 +0200

    Update ai4artsed_random_language_selector.py
    corrected language list

--- ai4artsed_random_language_selector.py
+++ ai4artsed_random_language_selector.py
@@ -11,18 +11,22 @@ class ai4artsed_random_language_selector:
     RETURN_NAMES = tuple([f"language_{i+1}" for i in range(12)])
     FUNCTION = "select_languages"
     CATEGORY = "AI4ArtsEd"
-
     def select_languages(self):
         supported_languages = [
-            "English", "French", "German", "Spanish", "Italian", "Portuguese",
-            "Dutch", "Russian", "Chinese", "Japanese", "Korean", "Arabic",
-            "Hindi", "Bengali", "Turkish", "Vietnamese", "Polish", "Ukrainian",
-            "Romanian", "Czech", "Greek", "Hungarian", "Swedish", "Finnish",
-            "Norwegian", "Danish", "Hebrew", "Thai", "Indonesian", "Malay",
-            "Yoruba", "Kiswahili", "Zulu", "Hausa", "Igbo", "Amharic",
-            "Somali", "Afrikaans", "Xhosa", "Sesotho", "Twi", "Shona",
-            "Kinyarwanda", "Luganda", "Wolof", "Fula", "Bambara", "Ewe",
-            "Lakota", "Māori", "Quechua", "Aymara", "Guarani", "Mapudungun"
+            "English", "Mandarin Chinese", "Hindi", "Spanish", "French", "Arabic",
+            "Bengali", "Russian", "Portuguese", "Indonesian", "Urdu", "German",
+            "Japanese", "Swahili", "Marathi", "Telugu", "Turkish", "Korean",
+            "Tamil", "Vietnamese", "Italian", "Gujarati", "Persian", "Bhojpuri",
+            "Hausa", "Kannada", "Maithili", "Burmese", "Punjabi", "Sunda",
+            "Ukrainian", "Igbo", "Uzbek", "Amharic", "Oromo", "Azerbaijani",
+            "Sinhala", "Kurdish", "Nigerian Pidgin", "Nepali", "Khmer", "Somali",
+            "Chittagonian", "Zulu", "Malay", "Pashto", "Lao", "Kinyarwanda",
+            "Czech", "Greek", "Chhattisgarhi", "Hungarian", "Haryanvi", "Kazakh",
+            "Xhosa", "Haitian Creole", "Akan", "Yoruba", "Uighur", "Shona",
+            "Balochi", "Konkani", "Assamese", "Tagalog", "Thai", "Polish",
+            "Dutch", "Romanian", "Cebuano", "Serbo-Croatian", "Malagasy",
+            "Hebrew", "Swedish", "Danish", "Norwegian", "Finnish", "Bulgarian",
+            "Slovak", "Lithuanian", "Latvian"
         ]
         selected_languages = random.sample(supported_languages, 12)
         return tuple(selected_languages)
