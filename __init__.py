41722f068ac392c42bfc996de5f8e67666350866
Prof. Dr. Benjamin Jörissen <joerissen@gmail.com>
2025-06-26 20:59:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from .ai4artsed_openrouter_key import ai4artsed_openrouter_key
 from .ai4artsed_t5_clip_fusion import ai4artsed_t5_clip_fusion
 from .ai4artsed_prompt_interception import ai4artsed_prompt_interception
 from .ai4artsed_image_analysis import ai4artsed_image_analysis
+from ai4artsed_audio_ldm2 import ai4artsed_audioldm2
 
 NODE_CLASS_MAPPINGS = {
     "ai4artsed_text_remix": ai4artsed_text_remix,
@@ -16,6 +17,7 @@ NODE_CLASS_MAPPINGS = {
     "ai4artsed_t5_clip_fusion": ai4artsed_t5_clip_fusion,
     "ai4artsed_prompt_interception": ai4artsed_prompt_interception,
     "ai4artsed_image_analysis": ai4artsed_image_analysis,
+    "ai4artsed_audio_ldm2": ai4artsed_audioldm2,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -27,4 +29,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ai4artsed_t5_clip_fusion": "AI4ArtsEd T5‑CLIP Fusion",
     "ai4artsed_prompt_interception": "AI4ArtsEd Prompt Interception",
     "ai4artsed_image_analysis": "AI4ArtsEd Image Analysis",
+    "ai4artsed_audio_ldm2": "AI4ArtsEd Audio ldm2"
 }
